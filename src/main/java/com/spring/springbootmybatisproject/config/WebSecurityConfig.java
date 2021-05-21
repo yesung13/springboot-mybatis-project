87d332b14edf6f76b087112ec8612b9709c5e50b
yys <berno579@gmail,com>
2021-05-21 16:06:34 +0900

    interceptor 관련 class 전체 주석 처리, Spring Security config class 생성

---
+++ src/main/java/com/spring/springbootmybatisproject/config/WebSecurityConfig.java
@@ -0,0 +1,15 @@
+//package com.spring.springbootmybatisproject.config;
+//
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//
+//public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+//
+//    private final UserService userService;
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.authorizeRequests()
+//                .antMatchers("/login")
+//    }
+//}
